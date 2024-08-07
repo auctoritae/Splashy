b8c020bc6b7e3c3dc82665e9ef5cd97ba91adfc4
axlobanoff <axlobanoff@icloud.com>
2024-08-07 20:03:49 +0300

    Initial Commit

---
+++ Splashy/SplashyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplashyApp.swift
+//  Splashy
+//
+//  Created by Alexander Lobanoff on 8/7/24.
+//
+
+import SwiftUI
+
+@main
+struct SplashyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
